f4f1655cb65b6f91870c60e7eef80bc91b34c32e
Phillip <pcquipse@gmail.com>
2019-10-09 22:52:15 +0800

    Updates

---
+++ precache-manifest.acbbe359d0ca1c8747fbc4a9f3f0db24.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c2fe516c23c1d867d6e34e63ee16c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6950c3a1ede0568d8359",
+    "url": "/static/js/2.456e905c.chunk.js"
+  },
+  {
+    "revision": "a4af6a40ab1dff420775",
+    "url": "/static/js/main.d3ef7820.chunk.js"
+  },
+  {
+    "revision": "c3a266751b9b757a657d",
+    "url": "/static/js/runtime-main.eb156c4c.js"
+  }
+]);
--- src/components/Badge.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Badge = (props) => {
-    return(
-        <span className="badge badge-light text-dark shadow-sm mx-2 my-2 font-size-14" >
-            <span className={props.item.logo + " mr-1"}></span> {props.item.skill} {props.item.title}
-        </span>
-    )
-}
